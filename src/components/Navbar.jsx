3332781d8661fabadabfa08adf183293717798d7
Sunil Belbase <linusbelbase2058@gmail.com>
2024-11-14 18:54:18 +0545

    a

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,31 @@
-
 import logo from "../assets/logo.png";
-import {FaLinkedin } from "react-icons/fa";
-import {FaGithub } from "react-icons/fa";
-import {FaSquareXTwitter } from "react-icons/fa6";
-import {FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
-  return <nav className="mb-20 flex items-center justify-between py-0">
-    <div className="flex-shrink-0 items-center">
-        <img src={logo} alt="logo" className= "mx- w-15 h-12" />
-    </div>
-    <div className="m-8 flex items-center justify-centre gap-4 text-2xl"> a
-       <a href="https://github.com/Linus-77" target="_blank" rel="noopener noreferrer">
+  return (
+    <nav className="mb-20 flex items-center justify-between py-0">
+      <div className="flex-shrink-0 items-center">
+        <img src={logo} alt="logo" className="w-15 h-12" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://github.com/Linus-77" target="_blank" rel="noopener noreferrer">
           <FaGithub />
-      </a>
-        
-        <FaInstagram/>
-        <FaSquareXTwitter/>
-        <FaLinkedin/>
-    </div>
-  </nav>
-}
+        </a>
+        <a href="https://www.instagram.com/linus_.77/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <FaSquareXTwitter />
+        </a>
+        <a href="https://www.linkedin.com/in/sunil-belbase/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
