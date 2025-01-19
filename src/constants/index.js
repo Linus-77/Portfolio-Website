e2106070a1a133ba5bbc5b23b5302f4c1e7d5b09
Sunil Belbase <sunilbelbase2058@gmail.com>
2025-01-19 13:46:58 +0545

    new

--- src/constants/index.js
+++ src/constants/index.js
@@ -75,5 +75,5 @@ export const PROJECTS = [
 export const CONTACT = {
   address: "Kathmandu, Nepal ",
   phoneNo: "+977 9843807770",
-  email: "sunilbelbase3@gmail.com",
+  email: "sunilbelbase2058@gmail.com",
 };
